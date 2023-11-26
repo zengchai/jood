4af6d920134b360cdaeae72a2e4bfc954d753a5c
lazyyam <yyzhan020623@gmail.com>
2023-11-26 10:56:19 +0800

    merge lazyyam code auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,56 +1,57 @@
-<<<<<<< HEAD
 import 'package:cloud_firestore/cloud_firestore.dart';
-=======
 import 'package:cloud_firestore/cloud_firestore.dart'; //yam
->>>>>>> lazyyam
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:jood/models/users.dart';
 import 'package:jood/pages/Order/orderPage.dart'; //yam
 import 'package:jood/services/database.dart';
 
-class AuthService{
-
+class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // create user object based on firebaseuser
-  AppUsers? _userFromFirebaseUser(User user){
-    return user !=null ? AppUsers(uid: user.uid) : null;
+  AppUsers? _userFromFirebaseUser(User user) {
+    return user != null ? AppUsers(uid: user.uid) : null;
   }
 
   // auth change user stream
-  Stream<AppUsers> get changeuser{
-    return _auth.authStateChanges()
-      .map((User? user) => _userFromFirebaseUser(user!)!);
+  Stream<AppUsers> get changeuser {
+    return _auth
+        .authStateChanges()
+        .map((User? user) => _userFromFirebaseUser(user!)!);
   }
 
   // sign in anon
-Future signInAnon() async {
-  try{
-    UserCredential result  = await _auth.signInAnonymously();
-    User? users = result.user;
-
-    return _userFromFirebaseUser(users!);
-  }
-  catch(e){
-    print(e.toString());
-    return null;
-  }
-}
-  // sign in with email & password
-  Future signInWithEmailAndPassword (String email, String password) async {
-    try{
-      UserCredential result  = await _auth.signInWithEmailAndPassword(email: email, password: password);
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
       User? users = result.user;
+
       return _userFromFirebaseUser(users!);
-    } catch(e){
+    } catch (e) {
       print(e.toString());
       return null;
     }
   }
+
+  // sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? users = result.user;
+      return _userFromFirebaseUser(users!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   // register with email & password
-  Future registerWithEmailAndPassword (String email, String password, String name) async {
-    try{
-      UserCredential result  = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future registerWithEmailAndPassword(
+      String email, String password, String name) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       User? users = result.user;
 
       //create a new document for the new user with the uid
@@ -59,7 +60,7 @@ Future signInAnon() async {
       await DatabaseService(uid: users!.uid).updatePaymentData('TnG', '0.00');
       await DatabaseService(uid: users!.uid).updateReviewData('', '', '');
       return _userFromFirebaseUser(users);
-    } catch(e){
+    } catch (e) {
       print(e.toString());
       return null;
     }
@@ -90,13 +91,13 @@ Future signInAnon() async {
   }
 
   // sign out
-  Future signOut() async{
-      try{
-        return await _auth.signOut();
-      } catch(e){
-        print(e.toString());
-        return null;
-      }
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
   Future<void> deleteUserData(String uid) async {
@@ -110,7 +111,6 @@ Future signInAnon() async {
       await firestore.collection(collectionName).doc(uid).delete();
     }
   }
-
 
   Future deleteUserAccount() async {
     User? user = FirebaseAuth.instance.currentUser;
