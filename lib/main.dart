7c3e017d2fe3b62953ac5355d9fbb65e339b9645
lazyyam <yyzhan020623@gmail.com>
2023-12-03 11:03:48 +0800

    yam pull

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,13 @@ import 'package:provider/provider.dart';
 import 'pages/menu/provider/menu_provider/menu_provider.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure WidgetsBinding is initialized
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure WidgetsBinding is initialized
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   ); // Initialize Firebase
 
-
-runApp(
+  runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => MenuProvider()),
@@ -40,11 +40,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<AppUsers?>.value(
-      catchError: (User,user) {},
+      catchError: (User, user) {},
       value: AuthService().changeuser,
       initialData: null,
       child: MaterialApp(
-        routes: {'/authenticate': (context) => Authenticate(),
+        routes: {
+          '/authenticate': (context) => Authenticate(),
           '/home': (context) => Home(),
           '/signin': (context) => SignIn(),
           '/signup': (context) => Register(),
@@ -52,8 +53,6 @@ class MyApp extends StatelessWidget {
           '/editprofile': (context) => EditProfile(),
           '/method': (context) => MethodPage(),
           '/receipt': (context) => Receipt(),
-
-
         },
         home: const Wrapper(),
       ),
